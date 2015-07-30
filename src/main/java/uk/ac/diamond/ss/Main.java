ebd521b8f358a4594b18bd5b531d2785f46f859d
Sam Hough <samuel.hough@diamond.ac.uk>
2015-07-30 21:25:12 +0100

    Start to wire up main

--- src/main/java/uk/ac/diamond/ss/Main.java
+++ src/main/java/uk/ac/diamond/ss/Main.java
@@ -1,10 +1,25 @@
 package uk.ac.diamond.ss;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import uk.ac.diamond.ss.domain.PersonReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("What you think you are doing dave");
+    public static void main(String[] args) throws Exception {
+        String filename = "problem.xlsx";
+        if (args.length > 0) {
+            filename = args[0];
+        }
+
+        InputStream inputStream = new FileInputStream(filename);
+        Workbook wb = WorkbookFactory.create(inputStream);
+        PersonReader pr = new PersonReader(wb.getSheet("candidate_preferences"));
+        System.out.println("X=" + pr.read());
     }
 
 }
