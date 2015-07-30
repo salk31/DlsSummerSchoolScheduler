fe42e47d594a0b71c4ffed7b18aa4dbc79220e33
Sam Hough <samuel.hough@diamond.ac.uk>
2015-07-30 19:48:26 +0100

    Build a fat jar to making running it easier

---
+++ src/main/java/uk/ac/diamond/ss/Main.java
@@ -0,0 +1,10 @@
+package uk.ac.diamond.ss;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("What you think you are doing dave");
+    }
+
+}
